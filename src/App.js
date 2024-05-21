2956ed4c49333901c5fbca652897c084faa0d359
Jananga Wijewardhana <kavinduwijewardhana1999@gmail.com>
2024-05-21 21:23:31 +0530

    add css to App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import './App.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const[time, setTime] = useState(0);
   const[running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if(running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
   return (
-    <>
-    <h1>stopwatch</h1>
-    <div>
-      <span>{("0" + Math.floor((time/60000)%60))}:</span>
-      <span>{("0" + Math.floor((time/1000)%60))}:</span>
-      <span>{("0" + ((time/10)%100))}</span>
+    <div className='max-w-md flex flex-col  items-center justify-center py-8'>
+      <h1 className='text-2xl font-semibold'>StopWatch</h1>
+      <div className='text-xl font-semibold py-4'>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+
+      <div className='w-1/3 max-w-sm flex flex-row justify-evenly'>
+        {running ? (
+          <button
+            className='border rounded-lg py-1 px-2.5'
+            onClick={() => { setRunning(false) }}
+          >
+            Stop
+          </button>
+        ) : (
+          <button
+          className='border rounded-lg py-1 px-3'
+          onClick={() => { setRunning(true) }}
+          >
+            Start
+          </button>
+        )
+        }
+
+        <button
+          className='border rounded-lg py-1 px-2.5' 
+          onClick={() => { setTime(0) }
+          }
+        >
+          Reset
+        </button>
+      </div>
     </div>
-    <div>
-      <button onClick={() => { setRunning(true) }}>Start</button>
-      <button onClick={() => { setRunning(false) }}>Stop</button>
-      <button onClick={() => { setTime(0) }}>Reset</button>
-    </div>
-    </>
   );
 }
 
